aabfd13e4a3c14267060b5a5ba9611064b616c5f
JonahK-B <36051322+JonahK-B@users.noreply.github.com>
2018-02-09 14:51:53 -0600

    S4.1
    Unsure if initialization is needed?

--- PS3.R
+++ PS3.R
@@ -17,3 +17,23 @@ PlayGame(x)
 
 ### S4 method
 
+setClass(Class = "door",
+         representation = representation(
+           door = "numeric"
+         ),
+         prototype = prototype(
+           door = c()
+         )
+)
+new("door")
+
+setValidity("door", function(x){
+  test1 <- (x@door==1 | x@door==2 | x@door==3)
+  if(!test1){return("@door is not a valid value")}
+}
+)
+## will work
+choice <- new("door", door = 1)
+##will not work
+choice <- new("door", door = 56)
+choice
