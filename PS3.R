1ef74a4a37ebc9f563de90f3563a175814bc40f5
JonahK-B <36051322+JonahK-B@users.noreply.github.com>
2018-02-09 15:04:30 -0600

    S4.2
    Game is working, initialzation method added

--- PS3.R
+++ PS3.R
@@ -25,15 +25,37 @@ setClass(Class = "door",
            door = c()
          )
 )
-new("door")
 
 setValidity("door", function(x){
   test1 <- (x@door==1 | x@door==2 | x@door==3)
   if(!test1){return("@door is not a valid value")}
 }
 )
+
+setMethod("initialize", "door", function(.Object, ...) {
+  value = callNextMethod()
+  validObject(value)
+  return(value)
+}
+)
 ## will work
 choice <- new("door", door = 1)
 ##will not work
 choice <- new("door", door = 56)
+## Hey it worked
 choice
+
+setGeneric("PlayGame", 
+           function(object) { 
+             standardGeneric("PlayGame") 
+           } ) 
+setMethod("PlayGame", "door", 
+          function(object){ 
+            a <- sample(1:3, 1)
+            print(a)
+            if(object@door == a){
+            return("You Win!") 
+            }
+            else return("You lose!")
+          } )
+PlayGame(choice)
